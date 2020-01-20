a42b4a29e0131afc6b2c6fe51fff76ebf9a9fdec
Sofia Vidal Urriza <sofiavu@MacBook-Pro-de-Sofia.local>
2020-01-20 17:18:56 +0100

    error handling

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express')
 const apiRoutes = require('./routes/api')
+const mongoose = require('mongoose') // Conectar BBDD
+const bodyParser = require('body-parser') // lectura de peticiones
+// const cors = require('cors') 
+
 
 const app = express()
 const port = process.env.PORT || '3000'
 
+// Conectar BBDD
+const mongoUri = process.env.MONGO_URI || 'mongodb://localhost:27017/openwebinars'
+mongoose.connect(mongoUri)
 
+
+// Para poder leer el cuerpo de una peticion
+app.use(bodyParser.json()) // Convertira el cuerpo en un objeto JSON.
+// app.use(cors())
 
 
 app.get('/', (req, res) => {
@@ -22,3 +33,15 @@ app.delete('/api/posts/:id', apiRoutes.deletePost)
 app.listen(port, () => {
     console.log(`[Express App] The app is listening on port: ${port}`)
 })
+
+
+// Gestionar Errores
+app.on('error', (err) => handleError)
+app.on('uncaughtException', (err) => handleError)
+app.on('unhandledRejection', (err) => handleError)
+
+function handleError(err) {
+    console.error(`[Error] ${err.message}`)
+    console.error(err.stack)
+}
+
