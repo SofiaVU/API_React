abe53381bb6582bc126d955814ae49fab3054729
Sofia Vidal Urriza <sofiavu@MacBook-Pro-de-Sofia.local>
2020-01-20 16:54:48 +0100

    CRUD operations created

---
+++ models/post.js
@@ -0,0 +1,12 @@
+// modelo para BBDD -- este modelo identifica un CURSO
+const mongoose = require('mongoose')
+
+const PostSchema = new mongoose.Schema({
+    title: String,
+    contents: Array,
+    image: String,
+    releaseDate: Date,
+    special: boolean
+})
+
+module.exports = mongoose.model('Post', PostSchema)
